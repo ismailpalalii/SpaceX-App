c21e045b87552c9525795628469b556f23c1ea34
Ismail Palali <ismail.palali.pp@gmail.com>
2022-01-17 23:02:29 +0300

    Initial Commit

---
+++ SpaceX App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceX App
+//
+//  Created by ismail palali on 17.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
